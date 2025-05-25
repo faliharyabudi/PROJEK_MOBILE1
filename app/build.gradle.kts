f410b0f8d35d9d2ae66d1991f127ed6c6b1b042b
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-05-25 21:41:34 +0700

    tambah navigasi dari MainActivity ke LoginActivity2
     Menggunakan ViewBinding untuk mengakses tombol btn1 dengan aman tanpa findViewById().
     Saat tombol login diklik, Intent digunakan untuk berpindah dari MainActivity ke LoginActivity2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,14 +26,21 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
+
 
 dependencies {
 
