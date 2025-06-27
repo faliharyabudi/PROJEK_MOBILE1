0b95c79a0faf2cfb3be82aca47089086c9497fa7
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-06-27 21:55:03 +0700

    Menambahkan efek hover tombol jawaban di Soal13Activity

--- app/src/main/java/com/example/projekmobile/Soal13Activity.kt
+++ app/src/main/java/com/example/projekmobile/Soal13Activity.kt
@@ -1,21 +1,38 @@
 package com.example.projekmobile
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class Soal13Activity : AppCompatActivity() {
+
+    private var selectedButton: Button? = null
+    private var selectedAnswer: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_soal13)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnA = findViewById<Button>(R.id.btn_a13)
+        val btnB = findViewById<Button>(R.id.btn_b13)
+        val btnC = findViewById<Button>(R.id.btn_c13)
+        val btnD = findViewById<Button>(R.id.btn_d13)
+
+        fun handleJawabanDipilih(button: Button, answer: String) {
+            selectedButton?.background = ContextCompat.getDrawable(this, R.drawable.button_shape)
+            selectedButton?.setTextColor(ContextCompat.getColor(this, R.color.putih))
+
+            button.setBackgroundResource(R.drawable.button_selected)
+            button.setTextColor(ContextCompat.getColor(this, R.color.hitam))
+
+            selectedButton = button
+            selectedAnswer = answer
         }
-        supportActionBar?.hide()
+
+        btnA.setOnClickListener { handleJawabanDipilih(btnA, "A") }
+        btnB.setOnClickListener { handleJawabanDipilih(btnB, "B") }
+        btnC.setOnClickListener { handleJawabanDipilih(btnC, "C") }
+        btnD.setOnClickListener { handleJawabanDipilih(btnD, "D") }
     }
 }
