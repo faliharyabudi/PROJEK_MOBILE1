10d322dc8045100a73f894b08f71fe4e606a6bdb
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2025-06-27 22:35:06 +0700

    Menambahkan navigasi img_back dan fitur dialog konfirmasi saat tombol kembali ditekan pada Soal13Activity

--- app/src/main/java/com/example/projekmobile/Soal13Activity.kt
+++ app/src/main/java/com/example/projekmobile/Soal13Activity.kt
@@ -65,5 +65,27 @@ class Soal13Activity : AppCompatActivity() {
                 toast.show()
             }
         }
+
+        // Navigasi Tombol Back
+        val btnBack = findViewById<ImageView>(R.id.img_back)
+
+        btnBack.setOnClickListener {
+            val dialogView = layoutInflater.inflate(R.layout.dialog_exit, null)
+            val dialog = android.app.AlertDialog.Builder(this)
+                .setView(dialogView)
+                .setCancelable(true)
+                .create()
+
+            val btnYakin = dialogView.findViewById<Button>(R.id.btn_yakin)
+            btnYakin.setOnClickListener {
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+                finish()
+            }
+
+            // Tampilkan dialog di tengah
+            dialog.show()
+        }
     }
 }
